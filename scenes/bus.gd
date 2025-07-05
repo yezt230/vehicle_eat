193cfbe1ed9c8d75da10194747258960263282d1
yezt230 <mjollnirsandwich@gmail.com>
2025-07-04 19:22:33 -0500

    Gulp animation setup, detecting collision with bus and playing gulp animation

--- scenes/bus.gd
+++ scenes/bus.gd
@@ -1,33 +1,20 @@
 extends RigidBody2D
-#
-#const GRAVITY: float = 150.0
-#@onready var on_floor_label = $OnFloorLabel
-#@onready var on_floor_value_str: String = ""
-#@onready var on_floor_switch: bool = false
-#
-#func _ready():
-	#floor_stop_on_slope = false
-#
-#
-#func _process(delta):
-	#if is_on_floor():
-		#on_floor_value_str = "true"
-		#if not on_floor_switch:
-			#on_floor_switch = true
-	#else:
-		#on_floor_value_str = "false"
-		#if on_floor_switch:
-			#on_floor_switch = false
-	#on_floor_label.text = "is on floor: " + on_floor_value_str
-#
-#
-#func _physics_process(delta):
-	#velocity.y += delta * GRAVITY
-	#
-	#var motion = velocity * delta
-	#
-	#if on_floor_switch:
-		#velocity.x = velocity.y
-	#
-	#velocity.x -= velocity.y
-	#move_and_slide()
+
+signal eaten
+
+func _ready():
+	connect("eaten", Callable(self, "_on_bus_eaten"))
+
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var contact_count = state.get_contact_count()
+	for i in range(contact_count):
+		var collider = state.get_contact_collider_object(i)
+		if collider is CharacterBody2D:
+			print("Collided with CharacterBody2D:")
+			emit_signal("eaten")
+			destroy()
+
+
+func destroy():
+	queue_free()
---
+++ scenes/macro_pred.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+@onready var animations = $AnimationPlayer
+
+
+func _ready():
+	var bus = get_parent().get_node("Bus")
+	bus.connect("eaten", Callable(self, "_on_bus_eaten"))
+
+
+func _on_bus_eaten():
+	print("Bus has been eaten! Responding in MacroPred.")
+	animations.play("gulp")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "gulp":
+		print("finished gulp animation")
+		animations.play("still")
